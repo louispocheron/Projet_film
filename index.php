274c25526170abe5c9902f4fbc46dec9390d406c
“Manon” <manon.mille.pro@gmail.com>
2021-07-01 16:42:55 +0200

    ajout de la page index.php pour tester la barre de recherche

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST">
+        <input type="search" name="s" placeholder="Recherche">
+        <input type="submit" name="envoyer">
+    </form>
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="pont.js"></script>
+</body>
+
+</html>
