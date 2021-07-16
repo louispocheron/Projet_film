4f45957f13b323e4e35612e4afd3434409bf5ff7
“Manon” <manon.mille.pro@gmail.com>
2021-07-16 13:24:46 +0200

    requete a b c

--- model.php
+++ model.php
@@ -27,27 +27,26 @@ $sth->execute();
 $test = $sth->fetchAll(PDO::FETCH_NUM);
 
 
-// requette pour l'affiche des films aléatoire sur la page d'acceuil + fonction random
+// requette pour l'affiche des films aléatoire sur la page d'acceuil
 
 
 $cover = $film_db->prepare(
     "SELECT film.image
     FROM film
-    WHERE film.id_film
-    "
+    WHERE film.id_film"
 );
 
 // Requête pour la page film avec trailer
 
 $movie_page = $film_db->prepare(
-    "SELECT film.titre, film.description_film, date_de_sortie.date_de_sortie, group_concat(genre.genre) as genre, group_concat(realisateur.realisateur) as realisateur, film.image, film.trailer
+    "SELECT film.titre, film.description_film, date_de_sortie.date_de_sortie, group_concat(DISTINCT genre.genre) as genre, group_concat(DISTINCT realisateur.realisateur) as realisateur, film.image, film.trailer
     FROM film
     INNER JOIN film_has_realisateur ON film_has_realisateur.film_id_film = film.id_film
     INNER JOIN realisateur ON realisateur.idrealisateur = film_has_realisateur.realisateur_idrealisateur
     INNER JOIN film_has_genre ON film_has_genre.film_id_film = film.id_film
     INNER JOIN genre ON genre.idgenre = film_has_genre.genre_idgenre
     INNER JOIN date_de_sortie ON date_de_sortie.iddate_de_sortie = film.date_de_sortie_iddate_de_sortie
-    WHERE film.affiche LIKE '???'
+    WHERE film.affiche LIKE '%$movie%'
     GROUP BY film.titre, film.description_film, film.image, film.trailer, film.date_de_sortie_iddate_de_sortie"
 );
 
@@ -58,7 +57,7 @@ $gender_page = $film_db->prepare(
 FROM film
 INNER JOIN film_has_genre ON film_has_genre.film_id_film = film.id_film
 INNER JOIN genre ON genre.idgenre = film_has_genre.genre_idgenre
-WHERE genre.genre = '$gender_name'");
+WHERE genre.genre LIKE '$gender_name'");
 
 
 
@@ -68,11 +67,27 @@ $director_page = $film_db->prepare(
     FROM film
     INNER JOIN film_has_realisateur ON film_has_realisateur.film_id_film = film.id_film
     INNER JOIN realisateur ON realisateur.idrealisateur = film_has_realisateur.realisateur_idrealisateur
-    WHERE realisateur.realisateur = '$director_name'"
+    WHERE realisateur.realisateur LIKE '$director_name'"
 );
 
+// Requête par année
+
 $years_page = $film_db->prepare(
-    "SELECT film.image
+    "SELECT film.titre 
+    FROM film 
+    INNER JOIN date_de_sortie ON date_de_sortie.iddate_de_sortie = film.date_de_sortie_iddate_de_sortie 
+    WHERE date_de_sortie.date_de_sortie LIKE '$year'"
+);
+
+$abc_page = $film_db->prepare(
+    "SELECT film.titre, film.description_film, date_de_sortie.date_de_sortie, group_concat(DISTINCT genre.genre) as genre, group_concat(DISTINCT realisateur.realisateur) as realisateur, film.image, film.trailer
     FROM film
-    INNER JOIN"
+    INNER JOIN film_has_realisateur ON film_has_realisateur.film_id_film = film.id_film
+    INNER JOIN realisateur ON realisateur.idrealisateur = film_has_realisateur.realisateur_idrealisateur
+    INNER JOIN film_has_genre ON film_has_genre.film_id_film = film.id_film
+    INNER JOIN genre ON genre.idgenre = film_has_genre.genre_idgenre
+    INNER JOIN date_de_sortie ON date_de_sortie.iddate_de_sortie = film.date_de_sortie_iddate_de_sortie
+    WHERE film.titre
+     LIKE '$Letter%'
+    GROUP BY film.titre, film.description_film, film.image, film.trailer, film.date_de_sortie_iddate_de_sortie"
 );
